8dea9f461a301ca7ef5e8150858825edb2e954e0
donellgill <donell.gill@outlook.com>
2021-04-21 16:55:16 -0400

    created testDrinkCard

--- index.js
+++ index.js
@@ -3,22 +3,61 @@ const baseURL = 'https://www.thecocktaildb.com';
 /*gets drinks object from api*/
 
 function getDrinks(){
-fetch(`${baseURL}` + `/api/json/v1/1/search.php?s=`)
-.then (res => res.json())
+    fetch(`${baseURL}` + `/api/json/v1/1/search.php?s=`)
+    .then (res => res.json())
 
-.then (json => console.log(json));
+    .then (json => json.drink.forEach((drink) => {
+        const drinkCard = createDrinkCard(drink);
+        drinkCard.appendChild(document.getElementById('card-box'))
+    }
+    ));
 }
 
-
-/*<div class="card" style="width: 18rem;">
-            <img class="card-img-top" src='https://unsplash.com/photos/TVOAbbLL050' alt="Margarita">
-            <div class="card-body">
-              <h5 class="card-title"></h5>Margarita
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">How to Make</a>
-            </div>*/
+/*function loadCard(){
 
 
-function createDrinkCard(){
-    const card = 
 }
+
+/*function createIngredient(strIngredient${value}){
+    for (var i = 1; i < 16; i++);{
+        document.createElement('li')
+    }if{strIngredient === null}
+}*/
+
+function createDrinkCard(drink){
+    const card = document.createElement('div')
+        img = document.createElement('img')
+        card_body = document.createElement('div')
+        h2 = document.createElement('h2')
+        ul = document.createElement('ul')
+        /*li = createIngredient();*/
+        p = document.createElement('p')
+
+        img.src = drink.strDrinkThumb + '/preview';
+        card_body;
+        h2.innerHTML = drink.strDrink;
+        ul.innerHTML = drink.strIngredient;
+        p.textContent = drink.strInstructions;
+
+        card.appendChild(img)
+        card.appendChild(card_body)
+        card.appendChild(h2)
+        card.appendChild(ul)
+        card.appendChild(p)
+
+        return card;
+
+}
+
+const testDrinkCard = {
+strDrink: "GG",
+strDrinkThumb: "https://www.thecocktaildb.com/images/media/drink/tqpvqp1472668328.jpg",
+strGlass: "Collins Glass",
+strIngredient1: "Galliano",
+strIngredient2: "Ginger ale",
+strIngredient3: "Ice",
+strIngredient4: null,
+strInstructions: "Pour the Galliano liqueur over ice. Fill the remainder of the glass with ginger ale and thats all there is to it. You now have a your very own GG."
+}
+
+createDrinkCard(testDrinkCard)
