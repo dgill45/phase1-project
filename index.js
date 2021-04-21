5b76aaa42f7616bd02633b98a560c8bc9cc693c5
donellgill <donell.gill@outlook.com>
2021-04-21 01:18:35 -0400

    created drink card and added list to html

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const baseURL = 'https://www.thecocktaildb.com';
 
+/*gets drinks object from api*/
+
 function getDrinks(){
-fetch("https://sportplaces.api.decathlon.com/api/v1/places?origin=-74.269,40.792&radius=20&sports=175")
+fetch(`${baseURL}` + `/api/json/v1/1/search.php?s=`)
 .then (res => res.json())
 
 .then (json => console.log(json));
+}
+
+
+/*<div class="card" style="width: 18rem;">
+            <img class="card-img-top" src='https://unsplash.com/photos/TVOAbbLL050' alt="Margarita">
+            <div class="card-body">
+              <h5 class="card-title"></h5>Margarita
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">How to Make</a>
+            </div>*/
+
+
+function createDrinkCard(){
+    const card = 
 }
